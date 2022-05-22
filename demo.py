765852f0e92f5136e509811cc62606d3e87c3f29
Kevin McAleer <kevinmcaleer@gmail.com>
2022-05-22 15:28:23 +0100

    updated from release 1

--- demo.py
+++ demo.py
@@ -1,12 +1,14 @@
 # Scroller Demo
 # Kevin McAleer May 2022
 
-from time import sleep
 from scroller import Scroller
-from fonts import JONNY_FIVE
+
+from time import sleep
 
 # create a message to display
-message = "Hey! @kevmac - Robot Makers"
+# message = "subs: 6668 "
+# message = "abdefghijklmnopqrstuvwxyz0123456789"
+message = "?/\<>()~'|.,"
 
 # create a scroller 0bject
 scroll = Scroller()
@@ -14,22 +16,16 @@ scroll = Scroller()
 # set the hue colour (0 is red etc)
 hue = 0
 
-font = JONNY_FIVE()
-scroll.clear()
-scroll.build_frame(message=message, font=font)
+
 while True or KeyboardInterrupt:
-    for position in range(16,-100,-1):
+    for position in range(16,-len(message*(5+1)),-1):
         if hue <=1 or hue == 0:
             hue += 0.01
         else: hue = 0
 #         if scroll.brightness ==1 or scroll.brightness >= 0.1:
 #             scroll.brightness -= 0.1
 #         else: scroll.brightness = 1
-
-#         scroll.show_message(message, position, hue, font)
-        
-        scroll.show_frame(position)
-        scroll.update(position)
+        scroll.show_message(message, position, hue)
         sleep(0.05)
 
 scroll.clear()
--- scroller.py
+++ scroller.py
@@ -1,13 +1,9 @@
 # scroller demo
 
-from fonts import JONNY_FIVE
+from fonts import *
 import picounicorn
 from time import sleep
 import math
-import framebuf
-
-FRAMEBUFFER_WIDTH = 100
-FRAMEBUFFER_HEIGHT = 7
 
 picounicorn.init()
 
@@ -20,7 +16,6 @@ class Scroller():
     hue = 1.0
     saturation = 1.0
     brightness = 1.0
-    position = 100
 
     def hsv2rgb(self, hue, sat, val):
         """ Returns the RGB of Hue Saturation and Brightnes values """
@@ -82,104 +77,11 @@ class Scroller():
             for row in range(7):
                 picounicorn.set_pixel(col, row, 0, 0, 0)
         
-    def zfill(self, text, digits):
-        """ fills a number string with leading zeros"""
-        filled_text = ''
-        str_length = len(text)
-        zeros_to_add = digits - str_length
-        
-        if str_length > 0:
-            zeros = ""
-            for _ in range(zeros_to_add):
-                zeros= zeros + "0"
-        filled_text = zeros + text
-#         print(f'text:{text}, digits: {digits}, filled_text: {filled_text}')
-        return filled_text
-        
-    def draw_char_to_frame(self, character, frame, font, glyph, pos):
-        length = font.widths.get(glyph)
-        
-#         print(f'length is:{length}')
-        for row in range(0,5):
-            char = str(bin(character[row]))
-#             print(f'character: {character}, char: {char}')
-            
-            for col in range (0,length):
-                x = col+self.offset+pos
-                y = row+1
-                
-                # clear gap
-                if x+1 < 16 and x+1 > -1:
-                    frame.pixel(x+1,y,1)
-#                     picounicorn.set_pixel(x+1, y, 0, 0, 0)
-                
-                # write pixel
-                if x < 16 and x > -1:
-                    character_string = str(bin(character[row])).replace('0b','')
-                    character_string = self.zfill(character_string,length)
-                    
-#                     print(f'char string: {character_string}')
-                    if character_string[col] == '1':
-#                         picounicorn.set_pixel(x, y, r, g, b)
-                        frame.pixel(x,y,1)
-                        
-                    else:
-#                         picounicorn.set_pixel(x, y, 0, 0, 0)
-                        frame.pixel(x,y,0)
-        
-        self.offset += length + self.gap
-        self.frame = frame
-#         return frame
-        
-    def update(self, position):
-#         self.position -= 1
-        self.position = position
-        if self.position <0:
-            self.position = 100
-        self.show_frame(position)
-        print(f"position is: {self.position}")
-    
-    def build_frame(self,font, message):
-        """ returns a frame buffer to be displayed by show_frame """
-        frame = framebuf.FrameBuffer(bytearray(100 * 7 * 1), 100, 7, framebuf.MONO_HMSB)
-        
-        for character in message:
-
-            if character in font.characters:
-
-                self.draw_char_to_frame(font.characters.get(character), frame, font, glyph=character, pos=1)                
-       
-        self.offset = 0
-        
-        self.frame = frame
-
-
-    def show_frame(self,pos):
-        frame = self.frame
-#         pos = self.position
-    
-        for row in range(FRAMEBUFFER_HEIGHT):
-            for col in range(FRAMEBUFFER_WIDTH):
-                print(f'col:{col}, row:{row}, position{pos}')
-                if col+pos < FRAMEBUFFER_WIDTH+pos and col+pos > 0:
-                    if frame.pixel(col,row) == 1:
-                        print(f'col: {col},row:{row}, pos:{pos}')
-                        picounicorn.set_pixel(col+pos,row,255,255,255)
-                    
-    def display_character(self, character, glyph, pos, font):
+    def display_character(self, character,pos):
         r,g,b = self.hsv2rgb(self.hue, self.saturation, self.brightness)
-        
-        #find the max length of the string
-#         length = self.__find_character_width(character)
- 
-#         print(f'widths: {font.widths.get(character)}')
-        length = font.widths.get(glyph)
-        
-#         print(f'length is:{length}')
         for row in range(0,5):
-            char = str(bin(character[row]))
-#             print(f'character: {character}, char: {char}')
-            
+            length = len(character[0])
+
             for col in range (0,length):
                 x = col+self.offset+pos
                 y = row+1
@@ -190,30 +92,199 @@ class Scroller():
                 
                 # write pixel
                 if x < 16 and x > -1:
-                    character_string = str(bin(character[row])).replace('0b','')
-                    character_string = self.zfill(character_string,length)
-                    
-#                     print(f'char string: {character_string}')
-                    if character_string[col] == '1':
+                    if character[row][col] == '1':
                         picounicorn.set_pixel(x, y, r, g, b)
                         
                     else:
                         picounicorn.set_pixel(x, y, 0, 0, 0)
         
-        self.offset += length + self.gap
+        self.offset += len(character[0]) + self.gap
        
     
-    def show_message(self, message, position, hue:None, font):
+    def show_message(self, message, position, hue:None):
         if hue is None:
             hue = 1.0
         self.hue = hue    
 #         print(f"message is {message}")
         for character in message:
 #             print(character)
-            if character in font.characters:
-#                 print(f"Character: {character}")
-                self.display_character(font.characters.get(character), character, position, font)                
-#            
+            if character == 'a':
+                self.display_character(a, position)
+            if character == 'b':
+                self.display_character(b, position)
+            if character == 'c':
+                self.display_character(c, position)
+            if character == 'd':
+                self.display_character(d, position)
+            if character == 'e':
+                self.display_character(e, position)
+            if character == 'f':
+                self.display_character(f, position)
+            if character == 'g':
+                self.display_character(g, position)
+            if character == 'h':
+                self.display_character(h, position)
+            if character == 'i':
+                self.display_character(i, position)
+            if character == 'j':
+                self.display_character(j, position)
+            if character == 'k':
+                self.display_character(k, position)
+            if character == 'l':
+                self.display_character(l, position)
+            if character == 'm':
+                self.display_character(m, position)
+            if character == 'n':
+                self.display_character(n, position)
+            if character == 'o':
+                self.display_character(o, position)
+            if character == 'p':
+                self.display_character(p, position)
+            if character == 'q':
+                self.display_character(q, position)
+            if character == 'r':
+                self.display_character(r, position)
+            if character == 's':
+                self.display_character(s, position)
+            if character == 't':
+                self.display_character(t, position)
+            if character == 'u':
+                self.display_character(u, position)
+            if character == 'v':
+                self.display_character(v, position)
+            if character == 'w':
+                self.display_character(w, position)
+            if character == 'x':
+                self.display_character(x, position)
+            if character == 'y':
+                self.display_character(y, position)
+            if character == 'z':
+                self.display_character(z, position)
+            if character == ' ':
+                self.display_character(space, position)
+            if character == '1':
+                self.display_character(one, position)
+            if character == '2':
+                self.display_character(two, position)
+            if character == '3':
+                self.display_character(three, position)
+            if character == '4':
+                self.display_character(four, position)
+            if character == '5':
+                self.display_character(five, position)
+            if character == '6':
+                self.display_character(six, position)
+            if character == '7':
+                self.display_character(seven, position)
+            if character == '8':
+                self.display_character(eight, position)
+            if character == '9':
+                self.display_character(nine, position)
+            if character == '0':
+                self.display_character(zero, position)
+            if character == '.':
+                self.display_character(fullstop, position)
+            if character == '@':
+                self.display_character(at, position)
+            if character == '!':
+                self.display_character(exclaimation, position)
+            if character == '#':
+                self.display_character(pound, position)
+            if character == '"':
+                self.display_character(speech, position)
+            if character == '$':
+                self.display_character(dollar, position)
+            if character == '%':
+                self.display_character(percentage, position)
+            if character == '&':
+                self.display_character(amplesand, position)
+            if character == '(':
+                self.display_character(left_bracket, position)
+            if character == ')':
+                self.display_character(right_bracket, position)
+            if character == '?':
+                self.display_character(question, position)
+            if character == '^':
+                self.display_character(carat, position)
+            if character == '_':
+                self.display_character(underscore, position)
+            if character == '<':
+                self.display_character(left_arrow, position)
+            if character == '>':
+                self.display_character(right_arrow, position)
+            if character == '/':
+                self.display_character(forward_slash, position)
+            if character == '\\':
+                self.display_character(back_slash, position)
+            if character == '=':
+                self.display_character(plus, position)
+            if character == '-':
+                self.display_character(minus, position)
+            if character == ':':
+                self.display_character(colon, position)
+            if character == ';':
+                self.display_character(semicolon, position)
+            if character == '=':
+                self.display_character(equals, position)
+            if character == '~':
+                self.display_character(tilda, position)
+            if character == "'":
+                self.display_character(single_quote, position)
+            if character == '|':
+                self.display_character(pipe, position)
+            if character == 'A':
+                self.display_character(A, position)
+            if character == 'B':
+                self.display_character(B, position)
+            if character == 'C':
+                self.display_character(C, position)
+            if character == 'D':
+                self.display_character(D, position)
+            if character == 'E':
+                self.display_character(E, position)
+            if character == 'F':
+                self.display_character(F, position)
+            if character == 'G':
+                self.display_character(G, position)
+            if character == 'H':
+                self.display_character(H, position)
+            if character == 'I':
+                self.display_character(I, position)
+            if character == 'J':
+                self.display_character(J, position)
+            if character == 'K':
+                self.display_character(K, position)    
+            if character == 'L':
+                self.display_character(L, position)
+            if character == 'M':
+                self.display_character(M, position)
+            if character == 'N':
+                self.display_character(N, position)
+            if character == 'O':
+                self.display_character(O, position)
+            if character == 'P':
+                self.display_character(P, position)
+            if character == 'Q':
+                self.display_character(Q, position)
+            if character == 'R':
+                self.display_character(R, position)
+            if character == 'S':
+                self.display_character(S, position)
+            if character == 'T':
+                self.display_character(T, position)
+            if character == 'U':
+                self.display_character(U, position)
+            if character == 'V':
+                self.display_character(V, position)
+            if character == 'W':
+                self.display_character(W, position)
+            if character == 'X':
+                self.display_character(X, position)
+            if character == 'Y':
+                self.display_character(Y, position)
+            if character == 'Z':
+                self.display_character(Z, position)
+                
         self.offset = 0
     
 
