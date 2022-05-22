a6cd4d55522c17a57ab98717e28ca41a3747fcf6
Kevin McAleer <kevinmcaleer@gmail.com>
2022-05-22 14:20:48 +0100

    making it more memory efficient, but slower?'

--- demo.py
+++ demo.py
@@ -3,9 +3,10 @@
 
 from time import sleep
 from scroller import Scroller
+from fonts import JONNY_FIVE
 
 # create a message to display
-message = "subs: 6668"
+message = "Hey! @kevmac - Robot Makers"
 
 # create a scroller 0bject
 scroll = Scroller()
@@ -13,16 +14,22 @@ scroll = Scroller()
 # set the hue colour (0 is red etc)
 hue = 0
 
-
+font = JONNY_FIVE()
+scroll.clear()
+scroll.build_frame(message=message, font=font)
 while True or KeyboardInterrupt:
-    for position in range(16,-len(message*(5+1)),-1):
+    for position in range(16,-100,-1):
         if hue <=1 or hue == 0:
             hue += 0.01
         else: hue = 0
 #         if scroll.brightness ==1 or scroll.brightness >= 0.1:
 #             scroll.brightness -= 0.1
 #         else: scroll.brightness = 1
-        scroll.show_message(message, position, hue)
+
+#         scroll.show_message(message, position, hue, font)
+        
+        scroll.show_frame(position)
+        scroll.update(position)
         sleep(0.05)
 
 scroll.clear()
