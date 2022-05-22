6ada8276b7ffcf7fa3274b2828493377c8f39043
Kevin McAleer <kevinmcaleer@gmail.com>
2022-05-22 15:36:59 +0100

    Added some message examples

--- demo.py
+++ demo.py
@@ -6,9 +6,9 @@ from scroller import Scroller
 from time import sleep
 
 # create a message to display
-message = "KEV"
+# message = "KEV"
 # message = "kev"
-# message = "Hey Robot Makers!"
+message = "Hey Robot Makers!"
 # message = "subs: 6668 "
 # message = "abdefghijklmnopqrstuvwxyz0123456789"
 # message = "?/\<>()~'|.,"
@@ -19,16 +19,19 @@ scroll = Scroller()
 # set the hue colour (0 is red etc)
 hue = 0
 
+scroll.clear()
 
 while True or KeyboardInterrupt:
     for position in range(16,-len(message*(5+1)),-1):
         if hue <=1 or hue == 0:
             hue += 0.01
         else: hue = 0
+        
+        # uncomment the next section to make the brightness pulse
 #         if scroll.brightness ==1 or scroll.brightness >= 0.1:
 #             scroll.brightness -= 0.1
 #         else: scroll.brightness = 1
         scroll.show_message(message, position, hue)
-        sleep(0.05)
+#         sleep(0.05)
 
 scroll.clear()
